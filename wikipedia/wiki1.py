7f8d40573c9177eff789ee81613f760140854476
Nafis Alam <nafisalam1711@gmail.com>
2021-09-05 00:43:42 +0530

    print brackets updated

--- wikipedia/wiki1.py
+++ wikipedia/wiki1.py
@@ -1,4 +1,4 @@
 import wikipedia
-print wikipedia.summary("Wikipedia")
+print(wikipedia.summary("Wikipedia"))
 # Wikipedia (/ˌwɪkɨˈpiːdiə/ or /ˌwɪkiˈpiːdiə/ WIK-i-PEE-dee-ə) is a collaboratively edited, 
 #multilingual, free Internet encyclopedia supported by the non-profit Wikimedia Foundation...
